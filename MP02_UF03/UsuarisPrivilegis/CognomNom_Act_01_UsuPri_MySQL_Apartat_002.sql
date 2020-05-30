8560d8f73ab65af8515561aed5071187846f6fce
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-30 16:48:56 +0200

    Update CognomNom_Act_01_UsuPri_MySQL_Apartat_002.sql

--- MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_002.sql
+++ MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_002.sql
@@ -1,3 +1,13 @@
+/ * 
+2. Proporciona els permisos per la BD videoclub al nou usuari:
+  a. Llistar dades.
+  b. Inserir dades.
+  c. Modificar dades.
+  d. Esborrar dades.
+  e. Executar funcions.
+Fitxer: CognomNom_Act_01_UsuPri_MySQL_Apartat_002.sql
+*/
+
 GRANT SELECT,INSERT,UPDATE,DELETE,EXECUTE
 ON    videoclub
 FOR 'pardojoan'@'localhost';
