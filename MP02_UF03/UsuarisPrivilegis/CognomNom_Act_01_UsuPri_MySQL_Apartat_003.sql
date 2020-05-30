04b76b96719f256a549436631391edb17785b63c
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-30 16:50:35 +0200

    Update CognomNom_Act_01_UsuPri_MySQL_Apartat_003.sql

--- MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_003.sql
+++ MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_003.sql
@@ -2,9 +2,5 @@
 3. Aplica els privilegis.
 Fitxer: CognomNom_Act_01_UsuPri_MySQL_Apartat_003.sql
 */
-   GRANT tipus_de_permis
-   ON database_name.table_name
-   TO 'usuari'@'localhost';
-/*
 
 FLUSH PRIVILEGES;
