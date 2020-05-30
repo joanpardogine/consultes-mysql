d32fc2feeab6ad06e51489879d36b3bd3a8aabe5
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-30 16:48:08 +0200

    Update CognomNom_Act_01_UsuPri_MySQL_Apartat_001.sql

--- MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_001.sql
+++ MP02_UF03/UsuarisPrivilegis/CognomNom_Act_01_UsuPri_MySQL_Apartat_001.sql
@@ -1,1 +1,6 @@
+/ * 
+1. Crea un usuari nou amb el teu cognomnom en el servidor local.
+Fitxer: CognomNom_Act_01_UsuPri_MySQL_Apartat_001.sql
+*/
+
 CREATE USER 'pardojoan'@'localhost' IDENTIFIED BY 'joan';
